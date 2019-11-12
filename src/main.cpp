a3e4e604ce867bab4ff46bbea9871a83dca40c86
Rodrigo Anjos de Souza <rodrigo.anjos.souza@usp.br>
2019-11-12 12:05:07 -0200

    Added Controlled Architecture for Aquisition Loop

--- src/main.cpp
+++ src/main.cpp
@@ -50,28 +50,46 @@ void USART_Transmit(uint8_t data)               // (p. 184)
   UDR0 = data;                                  // (p. 195)
 }
 
-ISR(ADC_vect)
+/* Timer 0 Comparator A Interrupt  */
+ISR(TIMER0_COMPA_vect)
 {
-  /* ADC Conversion Interrupt  */
+  /* Restart ADC Conversion */
+  ADCSRA |= (1 << ADSC)
   /*
-     As soon as the ADIF register is high,
-     this interrupt service is triggered
+      Set ports HIGH for Osciloscope Tracing
+      of the code
   */
-  ADC_SPL_COUNT++;
   SET(PORTB, 4);
-  /* Send conversion results via USART */
-                                 // (p. 265)
-  USART_Transmit(ADCH);
+  SET(PORTB, 5);
 
-
-  if(ADC_SPL_COUNT >= ADC_SPL_TH)
+  /* Check to see if conversion is complete */
+  if ( ! ( ADCSRA & (1 << ADIF)))
   {
-    USART_Transmit('\n');
-    ADC_SPL_COUNT = 0;
+    ADC_SPL_COUNT++;
+    /* Transmit Acquired Sample */
+    USART_Transmit(ADCH);
+    if (ADC_SPL_COUNT >= ADC_SPL_TH)
+    {
+      /* Transmit termination character */
+      /*
+          Termination Char will be responsible
+          for LabVIEW or other listening
+          applications organization while
+          listening Serial port. In receiving
+          a termination char the program willknow
+          that a data block has been transmitted.
+
+          Carefull choice of termination char will
+          reduce data loss. Choose a value that
+          wont coincide with ADCH values.
+      */
+      USART_Transmit('\n');
+
+      /* Reset ADC Sample Counter */
+      ADC_SPL_COUNT = 0;
+    }
   }
-
   CLR(PORTB, 4);
-
 }
 
 /* Main Code */
@@ -81,47 +99,63 @@ int main()
   /* Set Baudrate for TX & RX*/                         // (p. 183)
   UBRR0H = (BRC >> 8);
   UBRR0L = (BRC     );
-
-  /* Enable Transmitter*/
+  /* Enable Transmitter */
   UCSR0B = (1 << TXEN0);                                // (p. 183)
-
-  /*  */
+  /* Enable double speed USART operation */
   UCSR0A |= (1 << U2X0);
-
-  /* Set up frame size for TX & RX to 8-bit*/
+  /* Set up frame size for TX & RX to 8-bit */
   UCSR0C = (1<< UCSZ01) | (1<< UCSZ00);                 // (p. 198)
+
+
+  //* Steup Timer 0 *//
+  /* Clear Previous Configuration */
+  TCCR0A  = 0x00;
+  TCCR0B  = 0x00;
+  TIMSK0  = 0x00;
+  /* Put Timer 0 in CTC Mode*/                          // (p. 108, 100)
+  TCCR0A |= (0 << WGM00) | (1 << WGM01);
+  TCCR0B |= (0 << WGM02);
+  /*
+      Counter Frequency is determinded by
+      F_TIMER0 = F_CPU / (Prescaler*(OCR0A+1))
+  */
+  /* Setup Timer 0 Prescaler */
+  TCCR0B |= (0 << CS02) | (1 << CS01) | (0 << CS00);
+  /* Setup Output Compare Value */
+  /*
+     Getting 48.804kHz from this sketch, hence F_CPU must be 15.617280 MHz
+     a 382.720 kHz deviation from the specification. Therefore it is
+     generally speaking a good ideia to calibrate and compensate via OCR0A
+     the clock deviation.
+  */
+  OCR0A   = 39; // yields aproximatly 50kHz for my cristal
+  /* Setup interrupt Mask */
+  TIMSK0 |= (1 << OCIE0A);
+
+
 
   //* Setup ADC *//
   /* Clear ADCSRA & ADCSRB registers */                 // (p. 263)
   ADCSRA = 0x00;
   ADCSRB = 0x00;
   ADMUX  = 0x00;
-
   /* Set Analog pin */
   ADMUX |= (ADC0 & MUXMASK);                            // (p. 262)
-
   /* Set Reference Voltage */
   ADMUX |= (1 << REFS0);                                // (p. 262)
-
   /* Set ADC value alignment */
   ADMUX |= (1 << ADLAR);                                // (p. 262, 265)
-
   /* Set up ADC clock via prescaler */
   /* Fs = F_CPU/(13.5*prescaler) */
   ADCSRA |= (1 << ADPS2) | (0 << ADPS1) | (0 << ADPS0); // (p. 264, 255)
-
-  /* Set up ADC Auto Trigger Source to Free Running */
-  ADCSRB |= (0 << ADTS2) | (0 << ADTS1) | (0 << ADTS0); // (p. 265, 266, 253)
-
+  /* Set up ADC Auto Trigger Source to Timer 0 Comparator A */
+  ADCSRB |= (0 << ADTS2) | (1 << ADTS1) | (1 << ADTS0); // (p. 265, 266, 253)
   /* Enable ADC Auto Trigger Mode */
   ADCSRA |= (1 << ADATE);                               // (p. 264)
-
   /* Enable ADC Interrrupt when measurement is completed */
   ADCSRA |= (1 << ADIE);                                // (p. 264)
-
   /* Enable ADC */
   ADCSRA |= (1 << ADEN);                                // (p. 263)
-
   /* Start ADC Conversion */
   ADCSRA |= (1 << ADSC);                                // (p. 263)
 }
